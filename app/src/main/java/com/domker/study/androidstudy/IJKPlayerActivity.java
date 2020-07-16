e0ff2e942fe02e28cfc9f753aafa2b2b00c6e7fc
Shay P.C <3180102686@zju.edu.cn>
2020-07-16 16:54:34 +0800

    bug fixes on the Media Player (ijk part)

--- app/src/main/java/com/domker/study/androidstudy/IJKPlayerActivity.java
+++ app/src/main/java/com/domker/study/androidstudy/IJKPlayerActivity.java
@@ -25,13 +25,13 @@ public class IJKPlayerActivity extends AppCompatActivity {
     private SurfaceHolder holder;
     private SeekBar bar;
     private TextView duration;
-    private Long length=1l,progress=0l;
+    private Long length=null,progress=null;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        duration=findViewById(R.id.duration);
-        bar=findViewById(R.id.seek_bar);
+//        duration=findViewById(R.id.duration);
+//        bar=findViewById(R.id.seek_bar);
         setContentView(R.layout.layout_ijkplayer);
         setTitle("ijkPlayer");
 
@@ -76,44 +76,31 @@ public class IJKPlayerActivity extends AppCompatActivity {
 
     private void update_status(){
         Handler mHandler=new Handler();
-        progress=ijkPlayer.getCurrentPosition();
+        progress=ijkPlayer.getCurrentPosition()/1000;
         mHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
-
                 update_status();
             }
         },1000);
-        if(duration!=null&&bar!=null){
+        if(!ijkPlayer.isPlaying())return;
+        if(length==null||length==0)length=ijkPlayer.getDuration()/1000;
+        if(duration==null)duration=findViewById(R.id.duration);
+        if(bar==null)bar=findViewById(R.id.seek_bar);
+//        if(duration!=null&&bar!=null){
             duration.setText(progress/60+":"+progress%60);
             Log.i("time",progress.toString());
-            bar.setProgress((int)(100.0*progress/length));
-
-        }
-//        if(!ijkPlayer.isPlaying()|| progress==null||length==null||length==0)
-//            length=ijkPlayer.getDuration();
-//        else{
-//            runOnUiThread(new Runnable() {
-//                @Override
-//                public void run() {
-//
-//                }
-//            });
-//        }
-
-//        runOnUiThread(new Runnable() {
-//            @Override
-//            public void run() {
-//         progress=ijkPlayer.getCurrentPosition();
-
-//            }
-//
-//        });
+            bar.setProgress((int)(100*progress/length));
     }
     private String getVideoPath() {
 //        return "m/vwww.bilibili.coideo/BV1HQ4y1K7fy/";
 //        return "http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/sl.m3u8";
         return "android.resource://" + this.getPackageName() + "/" + R.raw.bytedance;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 
     @Override
